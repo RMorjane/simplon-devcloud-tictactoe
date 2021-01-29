46231f8408acf35bfa551f5f0e9005de35df9aa6
Morjane RHELLAB <71873995+RMorjane@users.noreply.github.com>
2021-01-29 14:44:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
-let ids = ['A1','B1','C1','A2','B2','C2','A3','B3','C3']
+let selected = 0
 
-ids.forEach(function(item){
-    let div_case = document.getElementById(item)
-    div_case.addEventListener("click",e=>{
-        let div = e.target
-        console.log(div.id)
-        div_case.innerHTML = "<h1>O</h1>"
-    })
+let playersList = [
+    {
+        name: "morjane",
+        text: "O",
+        color: "red"
+    },
+    {
+        name: "biben",
+        text: "X",
+        color: "black"
+    }
+]
+
+let alphaList = ['A','B','C']
+
+let controls = {
+    'A1': [],
+    'B1': [],
+    'C1': [],
+    'A2': [],
+    'B2': [],
+    'C2': [],
+    'A3': [],
+    'B3': [],
+    'C3': []
+}
+
+let cases = {}
+
+alphaList.forEach(function(alpha){
+    for(i=1;i<=3;i++){
+        let div_case = document.getElementById(`${alpha}${i}`)
+        if(div_case){
+            div_case.addEventListener("click",e=>{
+
+                let player = playersList[selected]
+                div_case.innerHTML = `<h1 class="case">${player["text"]}</h1>`
+                div_case.style.color = player["color"]
+                cases[div_case.id] = player["text"]
+                selected = 1 - selected
+
+                test_case(div_case.id)
+            })
+        }else{
+            console.log("div_case inexistante")
+        }
+    }
 })
+
+function test_case(id){
+    if(id){
+        let indice = id.substr(1)
+        console.log(indice)
+
+    }else{
+        console.log("id inexistant")
+    }
+}
