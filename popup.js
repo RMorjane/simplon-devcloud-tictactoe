2573cc641ecc7ca153a17de68a13f72920e7593e
RHELLAB Morjane <rhellab_morjane@yahoo.fr>
2021-02-03 01:57:13 +0100

    popup html + js + css

--- popup.js
+++
@@ -1,5 +0,0 @@
-// commande de déclenchement de la popup
-document.getElementById("btn-test").addEventListener("click", function(){document.getElementsByClassName("popup-center")[0].classList.add("active");});
-
-// commande de disparition de la popup
-document.getElementById("popup-new-game-btn").addEventListener("click", function(){document.getElementsByClassName("popup-center")[0].classList.remove("active");});
